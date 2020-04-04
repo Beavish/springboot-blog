33dde01958217e6c3a5cd0e5224aa1fadf7101ce
Beavish <kieran.philip.williams@gmail.com>
2020-04-04 18:43:24 +0100

    blog diplaying with proxy, no images

---
+++ blog/frontend/src/Blog.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Blog extends Component {
+    state = {  isLoading : true,
+        Posts : [] }
+        async componentDidMount(){
+            const response=await fetch('/posts/all');
+            const body = await response.json();
+            this.setState({Posts : body , isLoading: false});
+        }
+    render() { 
+        const {Posts , isLoading} = this.state;
+        if(isLoading) 
+            return (<div>Loading...</div>);
+        return (    <div>
+           
+            <h2>Posts</h2>
+            {
+                Posts.map( post => 
+                    <div id={post.id}><br/>
+                        {post.title}<br/>{post.content}
+                    </div>
+                    
+                )
+
+            }
+
+        </div> );
+    }
+}
+ 
+export default Blog;
